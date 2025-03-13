8aae5ef72963f74a6dc68ec9c395e52ce2f1fdcb
Swati Gautam <swati.g@turing.com>
2025-03-13 17:13:46 +0530

    Initial Commit

---
+++ SwiftUIControlDemo/SwiftUIControlDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIControlDemoApp.swift
+//  SwiftUIControlDemo
+//
+//  Created by Ruchi Kumar on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIControlDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
